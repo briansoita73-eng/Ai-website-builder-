3d3a96d6292901b2f8813627e2077121318d6872
briansoita73-eng <briansoita73@gmail.com>
2026-06-24 10:24:26 +0300

    Add registration and login functionality

--- script.js
+++ script.js
@@ -24,3 +24,61 @@ alert(
 
 closePayment();
 }
+function register(){
+
+let email =
+document.getElementById("regEmail").value;
+
+let password =
+document.getElementById("regPassword").value;
+
+localStorage.setItem("email",email);
+localStorage.setItem("password",password);
+
+alert("Registration Successful");
+
+window.location="login.html";
+}
+
+function login(){
+
+let email =
+document.getElementById("email").value;
+
+let password =
+document.getElementById("password").value;
+
+if(
+email===localStorage.getItem("email")
+&&
+password===localStorage.getItem("password")
+){
+window.location="dashboard.html";
+}
+else{
+alert("Invalid Login");
+}
+}
+
+function submitProject(){
+
+let business =
+document.getElementById("business").value;
+
+let type =
+document.getElementById("websiteType").value;
+
+let project =
+business + " - " + type;
+
+localStorage.setItem(
+"project",
+project
+);
+
+document.getElementById("projects")
+.innerHTML =
+"<p>"+project+"</p>";
+
+alert("Project Submitted");
+}
